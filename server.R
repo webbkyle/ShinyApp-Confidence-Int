bc11cca0bf2c12b65b2495aa12717caa40c90fab
Kyle Webb <kylepwebb1992@gmail.com>
2018-06-25 23:08:56 -0400

    Add files via upload

---
+++ server.R
@@ -0,0 +1,38 @@
+library(shiny)
+library(ggplot2)
+# Define server logic required to draw a histogram
+
+createCI<-function(M,m,MU,SIG,err){
+  CI = matrix(0,M,2)
+  Xbar = c()
+  for(j in 1:M){
+    samp = rnorm(m, MU, SIG)
+    xbar = mean(samp)
+    sighat = sqrt(t(samp-xbar)%*%(samp-xbar)/(m-1))
+    Xbar = append(Xbar, xbar)
+    CI[j,] = c(xbar-qnorm(1-err/2)*(sighat/sqrt(m)), xbar+qnorm(1-err/2)*(sighat/sqrt(m)))
+  }
+  dat = cbind(Xbar,CI)
+  return(dat)
+}
+
+
+shinyServer(function(input, output) {
+  
+plot_it <- reactiveValues(a=0)
+ci <- eventReactive(input$button,{
+  createCI(input$N,input$n,input$trumu,input$trusig,input$truerr)
+})
+
+observeEvent(input$button,{
+  output$Plot1 <- renderPlot({
+    df = data.frame(Sample = 1:input$N, Mean = ci()[,1])
+    ggplot(df, aes(x=Sample, y=Mean)) +
+      geom_errorbar(aes(ymin=ci()[,2], ymax=ci()[,3]), width=.1) +
+      geom_point(color = 'dodgerblue') +
+      geom_hline(yintercept=input$trumu, color = 'firebrick2')
+  })
+  output$Error <- renderText(paste0('Sampled Error: ',{sum(!(ci()[,2]<input$trumu & ci()[,3]>input$trumu))/input$N}))
+})
+
+})
---
+++ ui.R
@@ -0,0 +1,25 @@
+# Define UI for application that draws a histogram
+shinyUI(fluidPage(
+  
+  # Application title
+  titlePanel("Confidence Interval Simulation"),
+  
+  # Sidebar with a slider input for the number of components
+  sidebarLayout(
+    sidebarPanel(
+      numericInput("N","Number of Samples:", value = 100),
+      numericInput("n","Number of Subsamples", value = 10),
+      numericInput("trumu","True Mean", value = 5),
+      numericInput("trusig","True Standard Deviation", value = 1),
+      numericInput("truerr", "Theoretical Error Rate", value = 0.05, min = .01, max = .5, step = .01),
+      actionButton("button", "Plot")
+    ),
+    
+    
+    # Show a plot of the generated distribution
+    mainPanel(
+      plotOutput("Plot1"),
+      textOutput("Error")
+    )
+  )
+))
